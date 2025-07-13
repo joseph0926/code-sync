c4e650aaf654efd673d6ef6c8e329a383f88979a
joseph0926 <joseph0926@naver.com>
2025-07-13 09:39:53 +0900

    chore.husky: husky 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  ...compat.extends('prettier'),
   {
     files: ['**/*.{js,jsx,ts,tsx}'],
     plugins: {
